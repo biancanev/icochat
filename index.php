50fb6f55fa24ead28bc8f57cb115c4cabdc3fcc4
Biancano <bebostunner234@gmail.com>
2017-10-31 16:30:15 -0700

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,33 @@
         <div style="clear:both"></div>
     </div>
      
-    <div id="chatbox"></div>
+    <div id="chatbox">
+     <?
+session_start();
+ 
+function loginForm(){
+    echo'
+    <div id="loginform">
+    <form action="index.php" method="post">
+        <p>Please enter your name to continue:</p>
+        <label for="name">Name:</label>
+        <input type="text" name="name" id="name" />
+        <input type="submit" name="enter" id="enter" value="Enter" />
+    </form>
+    </div>
+    ';
+}
+ 
+if(isset($_POST['enter'])){
+    if($_POST['name'] != ""){
+        $_SESSION['name'] = stripslashes(htmlspecialchars($_POST['name']));
+    }
+    else{
+        echo '<span class="error">Please type in a name</span>';
+    }
+}
+?>
+ </div>
      
     <form name="message" action="">
         <input name="usermsg" type="text" id="usermsg" size="63" />
