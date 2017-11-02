c0ac66861bd8cb5843e539192179c10b50b3ca10
Biancano <bebostunner234@gmail.com>
2017-11-01 20:59:57 -0700

    Update login.php

--- login.php
+++ login.php
@@ -3,11 +3,14 @@
 <head>
 <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Quicksand" />
 <title>Potato Place Isochat</title>
-	<h1>Welcome to the Potato Place's Isochat, <?php echo $_POST["username"]; ?>!</h1>
+	<h1>Welcome to the Potato Place's Isochat, <i><strong><?php echo $_POST["username"]; ?></strong></i>!</h1>
 </head>
 <body>
 	<p>We regret to say that the Isochat is not yet ready. Please be patient!</p>
-	<button>Launch Isochat</button>
+	<form action="chat-application.php" method="get" id="form1">
+    <input type="submit" value="Launch the Isochat!" id="launch"></input>
+    </form>
+    
 </body>
 </html>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
@@ -19,7 +22,11 @@ $(document).ready(function(){
 });
 </script>
 <style>
-button {
-	color: #32CD32;
+#launch {
+	background-color: #32CD32;
+	color: #FFFFFF;
+	border-color: #228B22;
+	width: 120e;
+	height: 30e;
 	}
 </style>
